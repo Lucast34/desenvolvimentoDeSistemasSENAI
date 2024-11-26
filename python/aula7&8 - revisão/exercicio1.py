b9a9dcad552fa9202c2f0b0ad90c00f4c9408aad
VSRohod <89645335+VSRohod@users.noreply.github.com>
2024-11-26 11:44:50 -0300

    Update exercicio1.py

--- python/aula7&8 - revisão/exercicio1.py
+++ python/aula7&8 - revisão/exercicio1.py
@@ -2,3 +2,21 @@
 1) faça uma calculadora com as 4 operações 
 configuradas ( +,-,*,/)
 """
+
+num1 = int(input('Digite o 1º número: '))
+operador = input('Digite o operador')
+num2 = int(input('Digite o 2º número: '))
+
+match operador:
+    case '+':
+        resultado = num1 + num2
+    case '-':
+        resultado = num1 - num2
+    case '*':
+        resultado = num1 * num2
+    case '/':
+        resultado = num1 / num2
+    case _:
+        print('Digite um operador válido')
+
+print(f'RESULTADO : {resultado}')
