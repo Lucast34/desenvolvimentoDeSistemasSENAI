94945970438486034ca9f578c1a57e227c496433
VSRohod <89645335+VSRohod@users.noreply.github.com>
2024-12-05 10:58:22 -0300

    aula14

--- python/aula15 - P.O.O/material2.py
+++ python/aula15 - P.O.O/material2.py
@@ -1,4 +1,5 @@
 # UPER
+
 class Cliente:
     def __init__(self,nome,idade):
         self.nome = nome
@@ -8,7 +9,16 @@ class Cliente:
     def adicionarSaldo(self,valor):
         self.saldo += valor
         print(f'O valor de {valor} foi adicionado')
+    def removerSaldo(self,valor):
+        self.saldo -= valor
+        print(f'O valor de {valor} foi debitado')
+    def avaliar(self):
+        nota = float(input('Qual é a avaliação? (0 a 5 ✨)'))
+        self.avaliacao = nota
+        print(f'Sua nota atual é de {self.avaliacao} ✨')
 
 cliente = Cliente('Victor',21)
 cliente.adicionarSaldo(100)
+cliente.removerSaldo(10)
+cliente.avaliar()
 print(cliente.saldo , cliente.avaliacao)
